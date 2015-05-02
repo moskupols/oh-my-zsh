c82890eac9918bd4bf9819721df57f4688ea5b62
Feodor Alexeev <feodor.alexeev@gmail.com>
2015-05-02 04:16:49 +0300

    Edit my aliases

--- plugins/common-aliases/common-aliases.plugin.zsh
+++ plugins/common-aliases/common-aliases.plugin.zsh
@@ -3,52 +3,73 @@
 #
 
 # ls, the common ones I use a lot shortened for rapid fire usage
-alias l='ls -lFh'     #size,show type,human readable
-alias la='ls -lAFh'   #long list,show almost all,show type,human readable
+alias ls='ls --color=auto --group-directories-first'
+alias l='ls -X'
+alias la='ls -AX'
+alias ll='ls -lhX'
 alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
 alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
-alias ll='ls -l'      #long list
-alias ldot='ls -ld .*'
+alias ldot='ls -ld  .*'
 alias lS='ls -1FSsh'
 alias lart='ls -1Fcart'
 alias lrt='ls -1Fcrt'
 
 alias zshrc='$EDITOR ~/.zshrc' # Quick access to the ~/.zshrc file
+alias vimrc='$EDITOR ~/.vimrc'
+alias awerc='$EDITOR ~/.config/awesome/rc.lua'
 
 alias grep='grep --color'
 alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS} '
 
 alias t='tail -f'
 
+# because typing 'cd' is A LOT of work!!
+alias ..='cd ../'
+alias ...='cd ../../'
+alias ....='cd ../../../'
+alias .....='cd ../../../../'
+
+alias op='xdg-open >/dev/null &>/dev/null'
+alias pmru='lp -d future'
+
 # Command line head / tail shortcuts
-alias -g H='| head'
-alias -g T='| tail'
-alias -g G='| grep'
-alias -g L="| less"
-alias -g M="| most"
-alias -g LL="2>&1 | less"
-alias -g CA="2>&1 | cat -A"
-alias -g NE="2> /dev/null"
-alias -g NUL="> /dev/null 2>&1"
-alias -g P="2>&1| pygmentize -l pytb"
+# alias -g H='| head'
+# alias -g T='| tail'
+# alias -g G='| grep'
+# alias -g L="| less"
+# alias -g M="| most"
+# alias -g LL="2>&1 | less"
+# alias -g CA="2>&1 | cat -A"
+# alias -g NE="2> /dev/null"
+# alias -g NUL="> /dev/null 2>&1"
+# alias -g P="2>&1| pygmentize -l pytb"
 
 alias dud='du -d 1 -h'
+alias dush='du -sh'
 alias duf='du -sh *'
+
 alias fd='find . -type d -name'
 alias ff='find . -type f -name'
 
 alias h='history'
 alias hgrep="fc -El 0 | grep"
 alias help='man'
+alias j='jobs'
 alias p='ps -f'
 alias sortnr='sort -n -r'
 alias unexport='unset'
 
-alias whereami=display_info
+# alias whereami=display_info
 
-alias rm='rm -i'
-alias cp='cp -i'
-alias mv='mv -i'
+# alias rm='rm -i'
+# alias cp='cp -i'
+# alias mv='mv -i'
+
+for crap in evince eog $BROWSER ; do
+    alias $crap="$crap --gtk-no-debug=all 2>&1 >/dev/null" ;
+done
+
+alias vlc='vlc 2>&1 >/dev/null'
 
 # zsh is able to auto-do some kungfoo
 # depends on the SUFFIX :)
@@ -67,15 +88,12 @@ if is-at-least 4.2.0; then
     for ft in $_image_fts; do alias -s $ft=$XIVIEWER; done
   fi
 
-  _media_fts=(ape avi flv m4a mkv mov mp3 mpeg mpg ogg ogm rm wav webm)
-  for ft in $_media_fts; do alias -s $ft=mplayer; done
+  _media_fts=(ape avi flv mkv mov mp3 mpeg mpg ogg ogm rm wav webm)
+  for ft in $_media_fts ; do alias -s $ft=vlc ; done
 
   #read documents
-  alias -s pdf=acroread
-  alias -s ps=gv
-  alias -s dvi=xdvi
-  alias -s chm=xchm
-  alias -s djvu=djview
+  for ft in pdf ps dvi djvu ; do alias -s $ft=evince ; done
+#   alias -s chm=xchm
 
   #list whats inside packed file
   alias -s zip="unzip -l"
