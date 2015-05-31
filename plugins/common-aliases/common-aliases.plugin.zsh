ced7a242f3e790c955a6b081e31681ac8e02c9a3
Feodor Alexeev <feodor.alexeev@gmail.com>
2015-05-31 10:00:36 +0300

    Change some fotmats

--- plugins/common-aliases/common-aliases.plugin.zsh
+++ plugins/common-aliases/common-aliases.plugin.zsh
@@ -75,13 +75,11 @@ alias vlc='vlc 2>&1 >/dev/null'
 # depends on the SUFFIX :)
 if is-at-least 4.2.0; then
   # open browser on urls
-  if [[ -n "$BROWSER" ]]; then
-    _browser_fts=(htm html de org net com at cx nl se dk)
-    for ft in $_browser_fts; do alias -s $ft=$BROWSER; done
-  fi
+  _browser_fts=(htm html org net com ru at cx nl se dk dk php)
+  for ft in $_browser_fts ; do alias -s $ft=$BROWSER ; done
 
-  _editor_fts=(cpp cxx cc c hh h inl asc txt TXT tex)
-  for ft in $_editor_fts; do alias -s $ft=$EDITOR; done
+  _editor_fts=(cpp cxx cc c hh h inl asc txt TXT tex py)
+  for ft in $_editor_fts ; do alias -s $ft=$EDITOR ; done
 
   if [[ -n "$XIVIEWER" ]]; then
     _image_fts=(jpg jpeg png gif mng tiff tif xpm)
